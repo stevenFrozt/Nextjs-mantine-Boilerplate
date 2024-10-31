8af3229d67a922c9a79d39abf83337484724a5ec
PALAWANPAWNSHOP\SKREVIO <skrevio@palawanpawnshop.com>
2024-10-31 17:32:28 +0800

    added poppins font

--- src/Providers/ThemeProvider.tsx
+++ src/Providers/ThemeProvider.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import {
   ColorSchemeScript,
   createTheme,
@@ -9,11 +10,19 @@ import {
 } from "@mantine/core";
 import Head from "next/head";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+
+// const geistSans = localFont({
+//   src: "../fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+
 const geistMono = localFont({
   src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -24,7 +33,7 @@ export default function ThemeProvider({ children }: { children: ReactNode }) {
   const theme = mergeMantineTheme(
     DEFAULT_THEME,
     createTheme({
-      fontFamily: geistSans.style.fontFamily,
+      fontFamily: poppins.style.fontFamily,
       fontFamilyMonospace: geistMono.style.fontFamily,
     }),
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 import ThemeProvider from "@/Providers/ThemeProvider";
 
@@ -15,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="antialiased">
+      <body className={`antialiased `}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
